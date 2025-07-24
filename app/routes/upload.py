da7e698fcdd42ec58624593b796d10b8991872b8
Kawser Mia <111815469+kawser7x@users.noreply.github.com>
2025-07-25 03:23:25 +0600

    upload.py

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, UploadFile, File
+# app/routes/upload.py
+
+from fastapi import APIRouter, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
-async def homepage():
-    return "<h1>Music QC System</h1>"
-
-@router.post("/upload/audio/")
-async def upload_audio(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
